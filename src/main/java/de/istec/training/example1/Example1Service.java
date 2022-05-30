2cf9da99434a018a11e4dc74053870bd2e69e1a9
Thomas Pfaff <thomas.pfaff@istec.de>
2022-05-30 14:13:29 +0200

    add further documentation for Example1

--- src/main/java/de/istec/training/example1/Example1Service.java
+++ src/main/java/de/istec/training/example1/Example1Service.java
@@ -16,7 +16,9 @@ public class Example1Service {
         Group-Name | Value-Type | <Value-Month-1> | ... | <Value-Month-n>
         Group-1 | Reference | ...
 
-        The UI expects the data sorted by Group-Name, Value-Type (Reference, Character)
+        The UI expects the data sorted by Group-Name, Value-Type (Reference, Character). In the UI we want to highlight the cell
+        if the value has changed in comparison to the previous month. We also want to be able to render referenced values in a different way
+        than unreferenced values.
 
         TODO implement the logic without implicit loops
      */
